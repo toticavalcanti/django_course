bddffb05c663551e5f8480bc74271b898192ebe1
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-13 13:53:58 -0300

    add detail method in mysite/polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -16,3 +16,10 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
