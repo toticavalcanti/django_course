d224238d0994dab78c1ec28d3003a2981c5cc935
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-13 13:55:51 -0300

    alter detail method in mysite/polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 def index(request):
@@ -18,8 +18,5 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
