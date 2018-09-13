1c4c93965e900248735b1b53bdeb974d170422ed
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-13 13:33:07 -0300

    set mysite/polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
