8cac6cea0c13d18c631edea012789b38de88a05e
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-13 13:48:33 -0300

    index method in mysite/polls/views.py rewritten

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,10 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
