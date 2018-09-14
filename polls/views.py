7ea50a99918dee6d19b02b025d9f46708b2c7700
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-14 13:16:20 -0300

    create a real version of the vote method in mysite/polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -15,7 +15,22 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Mostra novamente o formulário de votação das perguntas.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Sempre retorna um HttpResponseRedirect depois de lidar com sucesso
+        # com dados via POST. Isso impede que os dados sejam postados duas vezes se
+        # o usuário clicar no botão voltar.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
