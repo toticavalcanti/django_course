4beca9834dccb27c2971eba08188a8a1858daa50
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-14 13:18:19 -0300

    set results method in mysite/polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -11,8 +11,8 @@ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
