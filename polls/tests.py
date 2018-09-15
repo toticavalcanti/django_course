cee28fa455c99fb3438fdf1e6ffadb98a959f345
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 12:53:42 -0300

    create some view tests

--- polls/tests.py
+++ polls/tests.py
@@ -32,5 +32,38 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        self.assertIs(recent_question.was_published_recently(), True)]
+
+    def create_question(question_text, days):
+        """
+        Create a question with the given 'question_text' and published the
+        given number of 'days' offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+    class QuestionIndexViewTests(TestCase):
+
+        def test_no_questions(self):
+            """
+            If no questions exist, an appropriate message is displayed.
+            """
+            response = self.client.get(reverse('polls:index'))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "No polls are available.")
+            self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+        def test_past_question(self):
+            """
+            Questions with a pub_date in the past are displayed on the
+            index page.
+            """
+            create_question(question_text="Past question.", days=-30)
+            response = self.client.get(reverse('polls:index'))
+            self.assertQuerysetEqual(response.context['latest_question_list'],['<Question: Past question.>'])
+
+
     
