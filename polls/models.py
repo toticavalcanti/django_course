04dd245703e73889c73a74ceb07323a4b7a4a80c
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 12:42:14 -0300

    create a new test called test_was_published_recently_with_recent_question in Question class inside of the mysite/polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -22,6 +22,15 @@ class Question(models.Model):
         old_question = Question(pub_date=time)
         self.assertIs(old_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
