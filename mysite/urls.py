4a9432a592ba18b0821e9858ebf6bd381642ab76
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 15:02:34 -0300

    fix line 5 of the mysite/polls/templates/polls/index.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.urls import path
 
-from . import views
+from polls import views
 
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('/', views.detail, name='detail'),
+    path('', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('/results/', views.results, name='results'),
+    path('results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('/vote/', views.vote, name='vote'),
+    path('vote/', views.vote, name='vote'),
 ]
