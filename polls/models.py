698891f2ca7593e2c6183b90401208f6848f6606
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 12:40:54 -0300

    create a new test called test_was_published_recently_with_old_question in Question class inside of the mysite/polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -13,6 +13,15 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
