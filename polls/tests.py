aa2c13d5696994699680ef9ca112e8338bfc2072
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 12:55:40 -0300

    create some view tests

--- polls/tests.py
+++ polls/tests.py
@@ -66,4 +66,33 @@ class QuestionModelTests(TestCase):
             self.assertQuerysetEqual(response.context['latest_question_list'],['<Question: Past question.>'])
 
 
-    
+        def test_future_question(self):
+            """
+            Questions with a pub_date in the future aren't displayed on
+            the index page.
+            """
+            create_question(question_text="Future question.", days=30)
+            response = self.client.get(reverse('polls:index'))
+            self.assertContains(response, "No polls are available.")
+            self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+        def test_future_question_and_past_question(self):
+            """
+            Even if both past and future questions exist, only past questions
+            are displayed.
+            """
+            create_question(question_text="Past question.", days=-30)
+            create_question(question_text="Future question.", days=30)
+            response = self.client.get(reverse('polls:index'))
+            self.assertQuerysetEqual(response.context['latest_question_list'],['<Question: Past question.>'])
+
+        def test_two_past_questions(self):
+            """
+            The questions index page may display multiple questions.
+            """
+            create_question(question_text="Past question 1.", days=-30)
+            create_question(question_text="Past question 2.", days=-5)
+            response = self.client.get(reverse('polls:index'))
+            self.assertQuerysetEqual(
+            response.context['latest_question_list'],['<Question: Past question 2.>', '<Question: Past question 1.>'])
