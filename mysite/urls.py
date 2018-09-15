8340005fed34a8f36efb0490f7ce78cddd69d0ab
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 14:41:53 -0300

    adding from django.contrib import admin to fix  line 23 of the mysite/mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,9 +19,9 @@ from polls import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/', views.DetailView, name='detail'),
+    path('<int:question_id>/results/', views.ResultsView, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
