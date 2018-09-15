9b6017a233eb110c87ad0ed0119b1e7f64e99137
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 13:58:20 -0300

    insert path('admin/', admin.site.urls), to fix mysite/mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from polls import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
