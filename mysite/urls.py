e31b2c599ce722585be3ab0d5dbd941357bcef8d
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 14:23:41 -0300

    adding from django.contrib import admin to fix  line 23 of the mysite/mysite/urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
+from django.contrib import admin
 from polls import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
