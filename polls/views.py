a01c7c395ddac2b431f3e293abd3d847bbc50c17
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 14:50:49 -0300

    set views.py methods

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,3 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Question
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
@@ -15,7 +7,3 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
