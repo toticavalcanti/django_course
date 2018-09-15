c3cb2b54d4ce78bc76f549edb593ddea88639677
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-15 15:31:54 -0300

    rewritten detail method in mysite/polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
