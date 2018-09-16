f7bc4ae244496c9ffdec61e7c5646963abb0dc6f
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-16 13:52:28 -0300

    create and set mysite_project/mysite/urls.py

---
+++ polls/urls.py
@@ -0,0 +1,29 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from polls import views
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('vote/', views.vote, name='vote'),
+]
