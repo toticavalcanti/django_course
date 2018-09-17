6c7a5f08cb9c8594ae93db37c1b1496e180ab04c
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-17 20:28:59 -0300

    improve mysite_project/polls/admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -4,10 +4,12 @@ from .models import Choice, Question
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
 admin.site.register(Question, QuestionAdmin)
