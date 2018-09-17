183c31d297a93b093366f19061d6627e0086598f
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-17 20:43:41 -0300

    some changes in mysite_project/polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -10,8 +10,16 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+
         now = timezone.now()
-        return now - timedelta(days=1) <= self.pub_date <= now
+
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+
+    was_published_recently.boolean = True
+
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
