78e997d1827e5e0cb8ed81009ae473534bddeae1
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-17 20:45:49 -0300

    some changes in mysite_project/polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -10,11 +10,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-
         now = timezone.now()
-
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
+        return now - timedelta(days=1) <= self.pub_date <= now
     was_published_recently.admin_order_field = 'pub_date'
 
     was_published_recently.boolean = True
