d0494214ea5ee5f5b8016ec300dd9233bc9164be
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-17 20:15:43 -0300

    change in mysite_project/polls/admin.py to split the form in groups

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
-admin.site.register(Question, QuestionAdmin)
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)g
