d33a8417a9deb48305bd840eec00051271bb7fea
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-17 20:22:56 -0300

    change ChoiceInline to TabularInline in mysite_project/polls/admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Choice, Question
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 class QuestionAdmin(admin.ModelAdmin):
