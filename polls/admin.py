242cc8ad4c0b69a6bce30d498b9c1f55d6795511
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-17 20:07:27 -0300

    alter the line 6 of the mysite_project/polls/admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
 admin.site.register(Question, QuestionAdmin)
