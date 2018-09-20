2cb62163710199d8b07ddc16e5a6efba4d5d992b
toticavalcanti <toticavalcanti@hotmail.com>
2018-09-20 19:02:04 -0300

    fix requirements.txt

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -83,6 +83,10 @@ DATABASES = {
     #     'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
     #     'PORT': '3306',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # add this
