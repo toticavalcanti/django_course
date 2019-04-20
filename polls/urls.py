8ac79e90a05f283fe0285f1bcfcda8a8944a848f
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2019-04-20 13:47:07 -0300

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('', views.detail, name='detail'),
+    path('/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('results/', views.results, name='results'),
     # ex: /polls/5/vote/
